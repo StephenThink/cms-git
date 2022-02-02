d835688d799bceb4efe7f36bfdb5f182aeb94bce
StephenThink <86749311+StephenThink@users.noreply.github.com>
2022-02-02 11:55:29 +0000

    Updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule
-        ->command('statamic:git:commit')
+        ->command('php please git:commit')
         ->everyMinute();
     }
 
